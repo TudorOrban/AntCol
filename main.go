df4388d14023fde5aa92e62f6cb445496c07cc0d
TudorOrban <tudororban2@gmail.com>
2026-01-03 16:52:21 +0200

    Initialize Project

---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"log"
+
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+)
+
+const (
+	screenWidth  = 800
+	screenHeight = 600
+)
+
+type World struct {
+	width, height int
+}
+
+type Game struct {
+	world *World
+}
+
+func (g *Game) Update() error {
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	ebitenutil.DebugPrint(screen, "Ant Colony Sim: Initialized")
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
+	return screenWidth, screenHeight
+}
+
+func main() {
+	game := &Game{
+		world: &World{
+			width:  screenWidth,
+			height: screenHeight,
+		},
+	}
+
+	ebiten.SetWindowSize(screenWidth, screenHeight)
+	ebiten.SetWindowTitle("Ant Colony Simulation")
+
+	if err := ebiten.RunGame(game); err != nil {
+		log.Fatal(err)
+	}
+}
